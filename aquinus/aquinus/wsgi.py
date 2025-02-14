32586bf6bbbf0daf5aeb34e430877ff016cbbe2a
EugenioZangara <eugeniozangara@gmail.com>
2025-02-14 07:34:04 -0300

    deploy

--- aquinus/aquinus/wsgi.py
+++ aquinus/aquinus/wsgi.py
@@ -11,6 +11,11 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
+# Añade la ruta del proyecto al path de Python
+path = '/ruta/a/tu/proyecto/aquinus'
+if path not in sys.path:
+    sys.path.append(path)
+    
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aquinus.settings.prod')
 
 application = get_wsgi_application()
