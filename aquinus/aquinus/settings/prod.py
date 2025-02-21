0cc1aec851da66d5e72f4aab44f00d060f26861a
EugenioZangara <eugeniozangara@gmail.com>
2025-02-21 08:20:37 -0300

    actualización prod.py

--- aquinus/aquinus/settings/prod.py
+++ aquinus/aquinus/settings/prod.py
@@ -35,45 +35,7 @@ DATABASES = {
     },
 }
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "aquinus",
-#         "USER": "ezangara",
-#         "PASSWORD": "Armada2024@",
-#         "HOST": "192.16.0.252",  # O la dirección IP de tu servidor MySQL
-#         "PORT": "3306",  # El puerto de tu servidor MySQL},
-#     },
-#     "id8": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "aspi",
-#         "USER": "ezangara",
-#         "PASSWORD": "Armada2024@",
-#         "HOST": "192.16.0.252",  # O la dirección IP de tu servidor MySQL
-#         "PORT": "3306",  # El puerto de tu servidor MySQL},
-#     },
-# }
-"""
-Configuración para trabajar OFFLINE
-"""
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "aquinus",
-#         "USER": "ezangara",
-#         "PASSWORD": "Armada2024@",
-#         "HOST": "localhost",  # O la dirección IP de tu servidor MySQL
-#         "PORT": "3306",  # El puerto de tu servidor MySQL},
-#     },
-#     "id8": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "aspi",
-#         "USER": "ezangara",
-#         "PASSWORD": "Armada2024@",
-#         "HOST": "localhost",  # O la dirección IP de tu servidor MySQL
-#         "PORT": "3306",  # El puerto de tu servidor MySQL},
-#     },
-# }
+ 
 
 
 # Static files (CSS, JavaScript, Images)
